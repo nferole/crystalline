d1497befaeb0c9be02a618cd3c5ffd9f4d799f10
Niklas Ferole <niklas@ferole.net>
2023-06-20 18:59:31 +0200

    Add QoL console extensions

---
+++ Ferole.CommandLineUtils/Features/Helpers/ConsoleHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferole.CommandLineUtils.ConsoleHelpers
+{
+    public static class CliExt
+    {
+        public static void WriteSameLine(string text)
+        {
+            Console.Write("\r"+new string(' ', Console.WindowWidth - 1)+"\r");
+            Console.Write(text);
+        }
+        public static void WriteSameLineColor(string text, ConsoleColor color)
+        {
+            try
+            {
+                Console.ForegroundColor = color;
+                Console.Write("\r" + new string(' ', Console.WindowWidth - 1) + "\r");
+                Console.Write(text);
+            }
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+    }
+}
